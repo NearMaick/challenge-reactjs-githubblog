40a580506bcc2fdbc10e3b6ac3eb77b16e5975db
Maick Souza <maick_a_s@msn.com>
2022-09-20 13:05:29 -0300

    style: making profile #1

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,5 +1,31 @@
-import styles from "./blog.module.css";
+import styles from "./styles.module.css";
 
 export function Blog() {
-  return <h1 className={styles.container}>Blog</h1>;
+  return (
+    <div className={styles.container}>
+      <main className={styles.content}>
+        <img
+          className={styles.image}
+          src='https://github.com/nearmaick.png'
+          alt=''
+        />
+        <div className={styles.profile}>
+          <div className={styles.profileContent}>
+            <h2>Maick Souza</h2>
+            <a href='#'>github</a>
+          </div>
+          <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan
+            disputationi eu sit. Vide electram sadipscing et per. Per aumento de
+            cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im
+            ampola pa arma uma pindureta.
+          </p>
+          <div className={styles.profileContent}>
+            <span>Maick Souza</span>
+            <span>32 seguidores</span>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
