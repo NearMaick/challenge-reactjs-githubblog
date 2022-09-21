24e907744616b658140871159eea409f3efe8cfb
Maick Souza <maick_a_s@msn.com>
2022-09-21 13:29:25 -0300

    feat: insert profile data

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,29 +1,41 @@
+import { useEffect, useState } from "react";
 import { PostCard } from "./components/PostCard";
 import styles from "./styles.module.css";
 
+interface Profile {
+  login: string;
+  name: string;
+  avatar_url: string;
+  bio: string;
+  followers: number;
+}
+
 export function Blog() {
+  const [profile, setProfile] = useState({} as Profile);
+
+  async function loadProfile() {
+    const response = await fetch("https://api.github.com/users/nearmaick");
+    const data = await response.json();
+    setProfile(data);
+  }
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.content}>
-        <img
-          className={styles.image}
-          src='https://github.com/nearmaick.png'
-          alt=''
-        />
+        <img className={styles.image} src={profile.avatar_url} alt='' />
         <div className={styles.profile}>
           <div className={styles.profileContent}>
-            <h2>Maick Souza</h2>
+            <h2>{profile.name}</h2>
             <a href='#'>github</a>
           </div>
-          <p>
-            Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan
-            disputationi eu sit. Vide electram sadipscing et per. Per aumento de
-            cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im
-            ampola pa arma uma pindureta.
-          </p>
+          <p>{profile.bio}</p>
           <div className={styles.profileContent}>
-            <span>Maick Souza</span>
-            <span>32 seguidores</span>
+            <span>@{profile.login}</span>
+            <span>{profile.followers} seguidores</span>
           </div>
         </div>
       </main>
